7e948eab97ec4fff7e3dee46ce2ddda93ff66d00
Nikolas <kolemazur@gmail.com>
2022-05-27 15:37:39 -0600

    Add articles route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -17,10 +17,42 @@ router.get('/', async (req, res) => {
       ],
     });
 
+    // Serialize
     const articles = articleData.map((article) => article.get({ plain: true }));
 
     res.render('homepage', { 
       articles, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/articles/:id', async (req, res) => {
+  try {
+    const articleData = await Article.findByPk(req.params.id, {
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'content', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    const articleSingle = articleData.get({ plain: true });
+
+    res.render('viewArticle', { 
+      articleSingle, 
       logged_in: req.session.logged_in 
     });
   } catch (err) {
