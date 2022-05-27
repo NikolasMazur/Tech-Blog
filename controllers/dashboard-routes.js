d271d153720b6612d6a6da952407dbec8af1a01a
Nikolas <kolemazur@gmail.com>
2022-05-27 17:12:01 -0600

    Add article update route

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -34,4 +34,25 @@ router.get('/newArticle', withAuth, async (req, res) => {
   }
 });
 
+router.get('/updateArticle/:id', withAuth, async (req, res) => {  
+  try {
+    const articleData = await Article.findOne({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    const article = articleData.get({ plain: true });
+
+    res.render('updateArticle', {
+      article,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
