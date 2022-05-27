fb29f5bce122ddefabde563a216dec8d8ab88775
Nikolas <kolemazur@gmail.com>
2022-05-27 14:55:30 -0600

    Add router

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,4 +2,31 @@ const router = require('express').Router();
 const { Article, Comment, User} = require('../models');
 const withAuth = require('../utils/auth');
 
+router.get('/', async (req, res) => {
+  try {
+    const articleData = await Article.findAll({
+      include: [
+        {
+          model: Comment,
+          attributes: ['content'],
+        },
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    const articles = articleData.map((article) => article.get({ plain: true }));
+
+    res.render('homepage', { 
+      articles, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
