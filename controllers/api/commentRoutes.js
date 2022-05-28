eb1b251e6236e4ebce590b7f75c1f52c924668d7
Nikolas <kolemazur@gmail.com>
2022-05-27 23:48:57 -0600

    Add comment delete

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -91,4 +91,25 @@ router.put('/:id', withAuth, async (req, res) => {
   }
 });
 
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!commentData) {
+      res.status(404).json({ message: 'No matching ID.' });
+      return;
+    }
+
+    res.status(200).json(commentData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
