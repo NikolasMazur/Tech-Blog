ec2a501f34940e2dc504602306c8d6585f0cd457
Nikolas <kolemazur@gmail.com>
2022-05-27 23:41:20 -0600

    Add comment get

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -33,4 +33,23 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:id', withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.findOne({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+    if (!commentData) {
+      res.status(404).json({ message: 'No matching ID.' });
+      return;
+    }
+    res.status(200).json(commentData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
