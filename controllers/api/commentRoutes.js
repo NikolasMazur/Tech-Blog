197fb5dcb2eafb60cedb4d32919610d55aa0d8af
Nikolas <kolemazur@gmail.com>
2022-05-27 23:37:34 -0600

    Add comment get

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,4 +2,35 @@ const router = require('express').Router();
 const { Comment, Article, User} = require('../../models');
 const withAuth = require('../../utils/auth');
 
+router.get('/', async (req, res) => {
+  try {
+    const commentData = await Comment.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+        {
+          model: Article,
+          attributes: ['username'],
+        },
+      ],
+      where: {
+          article_id: req.params.id,
+        },
+      order: [['created_at', 'ASC']],
+    });
+
+    const comments = commentData.map((comment) => comment.get({ plain: true }));
+
+    res.render('comments', { 
+      comments, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
